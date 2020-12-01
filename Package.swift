27b6a28f045b0c68be23676746ca479f3d2b0357
Brad Howes <bradhowes@mac.com>
2020-12-02 00:28:27 +0100

    Wrote README

--- Package.swift
+++ Package.swift
@@ -6,30 +6,17 @@ import PackageDescription
 let package = Package(
     name: "Checkbox",
     platforms: [
+         // could be smaller but this is min for SnapshotTesting
         .iOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Checkbox",
-            targets: ["Checkbox"]),
+        .library(name: "Checkbox", targets: ["Checkbox"]),
     ],
     dependencies: [
         .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.8.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Checkbox",
-            dependencies: []),
-        .testTarget(
-            name: "CheckboxTests",
-            dependencies: [
-                "Checkbox",
-                "SnapshotTesting"
-            ],
-            exclude: ["__Snapshots__"]
-        ),
+        .target(name: "Checkbox", dependencies: [], exclude: ["Checkbox.playground"]),
+        .testTarget(name: "CheckboxTests", dependencies: ["Checkbox", "SnapshotTesting"], exclude: ["__Snapshots__"]),
     ]
 )
